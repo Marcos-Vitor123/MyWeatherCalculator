9f5bc6afebc4d9e97d8ba9fc7566ff933360e120
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-03-19 19:09:20 -0300

    Ediçoes planejamento + Criando cabeçalho

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace MyWeatherCalculator
     {
         private static void Main(string[] args)
         {
+            Menu();
             // Continuação em Breve!
+        }
+
+        // Declarações das variáveis
+
+        public static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("\n\tBem vindo a Calculadora do Tempo!\t\n");
+            Console.ReadKey();
         }
     }
 }
