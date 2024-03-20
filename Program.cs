adf18467ec85a0f07d94d1390ddfafc7b0b39ce7
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-03-19 23:58:22 -0300

    Subtraindo tempo

--- Program.cs
+++ Program.cs
@@ -10,18 +10,28 @@ namespace MyWeatherCalculator
             // Continuação em Breve!
         }
 
-        public static int horas;
-        public static int minutos;
-        public static int segundos;
-        public static int tempoDisponivel;
-
-        TimeSpan Hora = new(60, 0, 0); // TimeSpan -> É uma estrutura. E aqui representa 60 horas
+        public static int removerDia = 1;
+        public static int subtrairMinuto = 1;
+        public static int horas = 24 - removerDia;
+        public static int minutos = 60;
+        public static int segundos = 60;
+        public static TimeSpan tempoDisponivelReal;
+        public static TimeSpan tempoDisponivel;
+        public static TimeSpan Hora = new(horas, minutos, segundos); // TimeSpan -> É uma estrutura. E aqui representa 60 horas
 
         public static void Menu()
         {
             Console.Clear();
             Console.WriteLine("\n\tBem vindo a Calculadora do Tempo!\t\n");
+            Operacao();
             Console.ReadKey();
+        }
+
+        public static void Operacao()
+        {
+            tempoDisponivel = Hora - TimeSpan.FromMinutes(30);
+            tempoDisponivelReal = tempoDisponivel - TimeSpan.FromMinutes(subtrairMinuto);
+            Console.WriteLine($"Tempo disponível: {tempoDisponivelReal}");
         }
     }
 }
