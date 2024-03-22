eb4f2d9a48b2e99c82f2e014c66ce8edb2c128ae
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-03-21 21:48:34 -0300

    Nova captura do tempo horas e minutos

--- Program.cs
+++ Program.cs
@@ -15,10 +15,12 @@ namespace MyWeatherCalculator
         public static int horas = 0; //- removerDia; // Evita aparecer dia no resultado Ex: 1.23:30:00
         public static int minutos = 0;
         public static int segundos = 0;
-        public static double removerSinal;
+        //public static double removerSinal;
         public static TimeSpan tempoDisponivel;
-        public static TimeSpan tempoDisponivelReal;
-        public static TimeSpan Hora = new(horas, minutos, segundos); // TimeSpan -> É uma estrutura. E aqui representa 60 horas
+        public static TimeSpan novoTempoHoras;
+        public static TimeSpan novoTempoMinutos;
+        //public static TimeSpan tempoDisponivelReal;
+        public static TimeSpan Hora = new(horas, minutos, segundos); // TimeSpan -> É uma estrutura.
 
         public static void Menu()
         {
@@ -32,13 +34,18 @@ namespace MyWeatherCalculator
         {
             Console.Write("Quantas horas você tem? ");
             horas = int.Parse(Console.ReadLine());
+            Console.Write("E quantos minutos você tem? ");
+            minutos = int.Parse(Console.ReadLine());
         }
         public static void Operacao()
         {
             CapturarDados();
-            tempoDisponivel = Hora - TimeSpan.FromHours(horas);
-            removerSinal = Math.Abs(tempoDisponivel.TotalHours); // remove sinal de menos do total de horas
-            tempoDisponivel = TimeSpan.FromHours(horas);
+            //tempoDisponivel = Hora - TimeSpan.FromHours(horas);
+            //removerSinal = Math.Abs(tempoDisponivel.TotalHours); // remove sinal de menos do total de horas
+            novoTempoHoras = TimeSpan.FromHours(horas);
+            novoTempoMinutos = TimeSpan.FromMinutes(minutos);
+            tempoDisponivel = novoTempoHoras + novoTempoMinutos;
+
             //tempoDisponivelReal = tempoDisponivel - TimeSpan.FromMinutes(subtrairMinuto);
             ImprimirTela();
         }
