42ffda5cb5308b9c2dd8fb29b56227ef86b3743d
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-03-24 19:02:10 -0300

    Criando a função criar- OBS: Não está pronto!

--- Program.cs
+++ Program.cs
@@ -7,11 +7,31 @@ namespace MyWeatherCalculator
         private static void Main(string[] args)
         {
             Menu();
+            Criar();
+            switch(opcao)
+            {
+                case 0:
+                {
+                    Menu();
+                    break;
+                }
+                case 1:
+                {
+                    Criar();
+                    break; 
+                }
+                default:
+                {
+                    Console.WriteLine("Opção inválida!");
+                    break;
+                }
+            }
             // Continuação em Breve!
         }
         public static int horas = 0; //- removerDia; // Evita aparecer dia no resultado Ex: 1.23:30:00
         public static int minutos = 0;
         public static int segundos = 0;
+        public static int opcao;
         public static double teste = 30;
         public static TimeSpan novoTeste;
         public static TimeSpan tempoDisponivel;
@@ -19,6 +39,9 @@ namespace MyWeatherCalculator
         public static TimeSpan tempoMinutos;
         public static TimeSpan tempoRestante;
         public static TimeSpan Hora = new(horas, minutos, segundos); // TimeSpan -> É uma estrutura.
+
+        public static List<string> nomeTarefa = [];
+        public static List<double> tempoTarefa = [];
 
         public static void Menu()
         {
@@ -52,6 +75,26 @@ namespace MyWeatherCalculator
         {
             Console.WriteLine($"Você tem {tempoDisponivel} disponível");
             Console.WriteLine($"Você tem {tempoRestante} restante");
+        }
+
+        public static void Criar()
+        {
+            Console.Clear();
+
+            Console.WriteLine("0 - Para terminar as tarefas.");
+            Console.WriteLine("1 - Para criar as tarefas.");
+
+            Console.Write("Digite o nome da tarefa: ");
+            nomeTarefa.Add(Console.ReadLine());
+            Console.Write("Digite o tempo dessa tarefa: ");
+            tempoTarefa.Add(double.Parse(Console.ReadLine()));
+
+            for (int i = 0; i < nomeTarefa.Count; i++)
+            {
+                Console.WriteLine($"{nomeTarefa[i]}: \t\t{tempoTarefa[i]}");
+            }
+
+            Console.ReadKey();
         }
     }
 }
